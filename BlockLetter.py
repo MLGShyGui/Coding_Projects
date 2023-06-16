20eade11305f40923b1b8e93ab1aae85daa71a87
MLGShyGui <99051746+MLGShyGui@users.noreply.github.com>
2023-06-15 20:22:54 -0400

    Create BlockLetter.py

---
+++ BlockLetter.py
@@ -0,0 +1,8 @@
+# This is part of the Codecademy.com Python3 course. No solution is provided, this is just my solution
+a_initial = """               A       L
+              A  A     L
+             A    A    L
+             AAAAAA    L 
+             A    A    L
+             A    A    LLLLLL"""
+print(a_initial)
