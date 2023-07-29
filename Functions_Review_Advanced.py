a98dfda22ba0eec233f8e9cc31430ec4d0367da4
MLGShyGui <99051746+MLGShyGui@users.noreply.github.com>
2023-07-29 23:32:53 +0000

    Update Functions_Review_Advanced.py

--- Functions_Review_Advanced.py
+++ Functions_Review_Advanced.py
@@ -54,4 +54,22 @@ print(dog_years("Baby", 0))
 # should print "Baby, you are 0 years old in dog years"
 
 
-print("#####   #####")
+print("#####  All Operations #####")
+# For the final code challenge, we are going to perform multiple mathematical operations on multiple inputs to the function. We will begin with adding the first two inputs,
+# then we will subtract the third and fourth inputs. After that, we will multiply the first result and the second result. 
+# In the end, we will return the remainder of the previous result divided by the first input. We will also print each of the steps.
+# Write your lots_of_math function here:
+def lots_of_math(a, b, c, d):
+  num1 = a + b
+  num2 = c - d
+  num3 = num1 * num2
+  num4 = num3 % a
+  print(num1)
+  print(num2)
+  print(num3)
+  return num4
+# Uncomment these function calls to test your lots_of_math function:
+print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
